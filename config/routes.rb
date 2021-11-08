3e53058ee9c65b98ddbcac299d50a6b14506041a
TedLivist <livistteddy@gmail.com>
2021-11-08 23:25:35 +0100

    Add react-router

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # Forward root to StaticController#requests
   root 'static#index'
   namespace :v1, defaults: { format: :json } do
     get 'things', to: 'things#index'
+  end
+
+  # Forward all requests to StaticController#index but requests
+  # must be non-Ajax(!req.xhr?) and HTML Mime type (req.format.html?).
+  # This does not include the root ("/") path.
+  get '*page', to: 'static#index', constraints: ->(req) do
+    !req.xhr? && req.format.html?
   end
 end
